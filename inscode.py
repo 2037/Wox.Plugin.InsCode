2cebd61b438bfe2b370304b99f64ce74384fa8d0
Jack Wang <me@mydomain.com>
2023-06-05 02:05:41 -0400

    fix apikey

--- inscode.py
+++ inscode.py
@@ -9,9 +9,10 @@ import requests
 
 class InsCode:
     def __init__(self, api_key=None):
-        self.api_key = os.getenv("OPENAI_API_KEY")
-        if api_key:
-            openai.api_key = self.api_key
+        if not api_key:
+            self.api_key = os.getenv("OPENAI_API_KEY")
+        else:
+            self.api_key = api_key
         openai.api_key = self.api_key
 
         with open('preference.json', 'r') as f:
@@ -21,6 +22,10 @@ class InsCode:
             self.is_keepitshort = self.preference["is_keepitshort"]
             self.max_tokens = self.preference["max_tokens"]
         self.presets = load_prompts()
+        self.is_clipboard = True
+        
+        # test mode
+        self.is_fakeanswer = False 
 
     # store preference as file since Wox terminate process after window closed
     def set_preset(self, preset):
@@ -54,6 +59,9 @@ class InsCode:
 
     def get_answer(self, question, **kwargs):
         # only return first codeblock, return original if failed
+        if self.is_fakeanswer:
+            return "This is a fake answer. \n"
+
         def extract_code(s):
             try:
                 pos = re.search(r"(```.*\n)", s)
@@ -71,6 +79,8 @@ class InsCode:
             answer = extract_code(answer_raw)
         else:
             answer = answer_raw
+        if self.is_clipboard:
+            pyperclip.copy(answer)
         return answer
 
     def test(self, question, preset_name=None, **kwargs):
@@ -86,6 +96,35 @@ class InsCode:
             print(preset_name, self.presets[preset_name], "\n", answer, "\n")
 
 
+
 if __name__ == "__main__":
+    import sys
     Bot = InsCode()
-    Bot.test("Write a hello world function", preset_name="Python")
+    # query mode
+    # python inscode.py "your question"
+    # Usage: subprocess.Popen(["python", "inscode.py", "query"])
+    if len(sys.argv) > 1:
+        if sys.argv[1]=="query":
+            while True:
+                question = sys.stdin.readline().strip()
+                if not question:
+                    break
+                answer = Bot.get_answer(question)
+                print("Received question: ", question)
+                print("Get answer: ", answer)
+
+    # interactive mode
+    # python inscode.py
+    else:
+        import time
+        start_time = time.time()
+        while True:
+            time_pass = time.time() - start_time
+            if time_pass > 300: # 5 minutes
+                break
+            question = input("Enter your question: (Type quit/q to quit)\n")
+            # question = input()
+            print(Bot.get_answer(question))
+            # print("Time pass: %d sec" % time_pass) 
+            if question == "quit" or question=="q": # Exit loop if user enters "quit"
+                break
